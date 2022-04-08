d3e5e408bcb589c3f5abad1b17ccab90a310dba8
Julio Arruda <julio.arruda@outlook.com>
2022-04-08 17:37:02 -0300

    add publish profile

--- appservice/outputs.tf
+++ appservice/outputs.tf
@@ -2,6 +2,10 @@ output "app_service_name" {
   value = "${azurerm_app_service.main.name}"
 }
 
+output "rg_name" {
+  value = "${azurerm_resource_group.rg.name}"
+}
+
 output "app_service_default_hostname" {
   value = "https://${azurerm_app_service.main.default_site_hostname}"
 }
